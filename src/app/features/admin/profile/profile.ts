a963a2b3817dd9b03e9a8472391e132f783d6820
thoratprashant <prashant.thorat@arkenea.com>
2026-01-14 01:10:03 +0530

    Working on profile page > admin

--- src/app/features/admin/profile/profile.ts
+++ src/app/features/admin/profile/profile.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [MatIconModule,CommonModule,MatButtonModule],
   templateUrl: './profile.html',
   styleUrl: './profile.scss',
 })

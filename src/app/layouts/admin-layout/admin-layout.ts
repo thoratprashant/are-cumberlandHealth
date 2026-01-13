70af52d55b136fc9d7d9cbb09f72d314be2d1a8f
thoratprashant <prashant.thorat@arkenea.com>
2026-01-13 21:38:47 +0530

    Working on Admin auth

--- src/app/layouts/admin-layout/admin-layout.ts
+++ src/app/layouts/admin-layout/admin-layout.ts
@@ -1,14 +1,25 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
+import {MatBadgeModule} from '@angular/material/badge';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [CommonModule,RouterOutlet],
+  imports: [CommonModule,RouterOutlet,MatIconModule, MatButtonModule, MatBadgeModule],
   templateUrl: './admin-layout.html',
   styleUrl: './admin-layout.scss',
 })
 export class AdminLayout {
+  mobileMenuOpen = signal(false);
 
+  toggleMenu() {
+    this.mobileMenuOpen.update(v => !v);
+  }
+
+  closeMenu() {
+    this.mobileMenuOpen.set(false);
+  }
 }

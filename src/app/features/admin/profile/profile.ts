852bdc6643b658371362d3fd234532ded1535f88
thoratprashant <prashant.thorat@arkenea.com>
2026-01-14 18:03:28 +0530

    Added Alert globally

--- src/app/features/admin/profile/profile.ts
+++ src/app/features/admin/profile/profile.ts
@@ -5,10 +5,12 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { ChangePassword } from '../../comman/change-password/change-password';
 import { EditPersonalInfo } from './edit-personal-info/edit-personal-info';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { AlertDialog } from '../../comman/alert-dialog/alert-dialog';
 
 @Component({
   selector: 'app-profile',
-  imports: [MatIconModule,CommonModule,MatButtonModule,MatDialogModule ],
+  imports: [MatIconModule,CommonModule,MatButtonModule,MatDialogModule,MatCheckboxModule ],
   templateUrl: './profile.html',
   styleUrl: './profile.scss',
 })
@@ -29,6 +31,20 @@ export class Profile {
       width: '610px',
       panelClass: 'modal--wrapper',
       autoFocus: false
+    });
+  }
+
+  alert() {
+    this.dialog.open(AlertDialog, {
+      width: '510px',
+      panelClass: 'modal--wrapper',
+      autoFocus: false,
+      data: {
+        title: 'Warning',
+        message: 'Are you sure you would like to proceed with this?',
+        button1: 'Yes',
+        button2: 'No'
+      }
     });
   }
 

62f6792220157090f5d9daa3ed623b2df1f163fe
thoratprashant <prashant.thorat@arkenea.com>
2026-01-14 11:53:23 +0530

    Profile COmpleted

--- src/app/features/admin/profile/profile.ts
+++ src/app/features/admin/profile/profile.ts
@@ -1,14 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { ChangePassword } from '../../comman/change-password/change-password';
 
 @Component({
   selector: 'app-profile',
-  imports: [MatIconModule,CommonModule,MatButtonModule],
+  imports: [MatIconModule,CommonModule,MatButtonModule,MatDialogModule ],
   templateUrl: './profile.html',
   styleUrl: './profile.scss',
 })
 export class Profile {
+
+  readonly dialog = inject(MatDialog);
+
+  changePassword() { 
+    this.dialog.open(ChangePassword, {
+      width: '610px',
+      panelClass: 'modal--wrapper',
+      autoFocus: false
+    });
+  }
 
 }

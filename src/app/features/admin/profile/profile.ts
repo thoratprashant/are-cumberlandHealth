e7dec12c2f447d8f8243c442b87c370257ebd001
thoratprashant <prashant.thorat@arkenea.com>
2026-01-14 12:33:26 +0530

    Added Personal Info modal

--- src/app/features/admin/profile/profile.ts
+++ src/app/features/admin/profile/profile.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { ChangePassword } from '../../comman/change-password/change-password';
+import { EditPersonalInfo } from './edit-personal-info/edit-personal-info';
 
 @Component({
   selector: 'app-profile',
@@ -17,6 +18,14 @@ export class Profile {
 
   changePassword() { 
     this.dialog.open(ChangePassword, {
+      width: '610px',
+      panelClass: 'modal--wrapper',
+      autoFocus: false
+    });
+  }
+
+  editPersonalInfo() { 
+    this.dialog.open(EditPersonalInfo, {
       width: '610px',
       panelClass: 'modal--wrapper',
       autoFocus: false

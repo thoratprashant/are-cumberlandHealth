bbef5000e1be930a3c129542b2a04c253b126087
thoratprashant <prashant.thorat@arkenea.com>
2026-01-15 10:32:28 +0530

    Set up done for auth and admin layout

--- src/app/layouts/admin-layout/admin-layout.ts
+++ src/app/layouts/admin-layout/admin-layout.ts
@@ -2,13 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import {MatBadgeModule} from '@angular/material/badge';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,MatIconModule, MatButtonModule, MatBadgeModule],
+  imports: [CommonModule,RouterOutlet,MatIconModule, MatButtonModule, MatBadgeModule,RouterModule],
   templateUrl: './admin-layout.html',
   styleUrl: './admin-layout.scss',
 })

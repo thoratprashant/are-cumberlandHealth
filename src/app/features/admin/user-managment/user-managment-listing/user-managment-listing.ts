ae3f9eaff32e0d63598ca9e6a9948ee8040968c7
thoratprashant <prashant.thorat@arkenea.com>
2026-01-16 09:50:11 +0530

    Working on User Managment

--- src/app/features/admin/user-managment/user-managment-listing/user-managment-listing.ts
+++ src/app/features/admin/user-managment/user-managment-listing/user-managment-listing.ts
@@ -1,11 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginator,MatPaginatorModule } from '@angular/material/paginator'; 
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+
+export interface UserData {
+  id: number;
+  name: string;
+  emailid: string;
+  role: string;
+  status: string;
+}
 
 @Component({
   selector: 'app-user-managment-listing',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule ,MatButtonModule, MatDialogModule,MatIconModule,MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule],
   templateUrl: './user-managment-listing.html',
   styleUrl: './user-managment-listing.scss',
 })
-export class UserManagmentListing {
+export class UserManagmentListing implements AfterViewInit {
 
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'emailid',
+    'role',
+    'status',
+    'action'
+  ];
+
+  dataSource = new MatTableDataSource<UserData>(USER_DATA);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
+
+/* ---------- DUMMY DATA (50 RECORDS) ---------- */
+
+const STATUS = ['Pending', 'Approved', 'Rejected'];
+
+const USER_DATA: UserData[] = Array.from({ length: 50 }).map((_, i) => ({
+  id: i + 1,
+  // name: `name ${i + 1}`,
+  name:['John Michael Smith', 'David Andrew Johnson','Robert James Miller', 'Daniel Thomas Wilson'][i % 4],
+  emailid: ['john24.michael@lorem.com', 'david.andrew@lorem.com', 'robert.james.miller@lorem.com', 'daniel.thomas.wilson@lorem.com'][i % 4],
+  role: ['Provider','Front Desk',][i % 2], 
+  status: STATUS[i % STATUS.length],
+}));

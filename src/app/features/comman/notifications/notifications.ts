9374d1e8dae0209e589455e30dc25cfef6152f4c
satyajitarkenea24 <satyajit@arkenea.com>
2026-01-19 14:21:26 +0530

    ui

--- src/app/features/comman/notifications/notifications.ts
+++ src/app/features/comman/notifications/notifications.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule, provideNativeDateAdapter } from '@angular/material/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-notifications',
-  imports: [],
+  providers: [provideNativeDateAdapter()],
+  imports: [CommonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule, ReactiveFormsModule],
   templateUrl: './notifications.html',
   styleUrl: './notifications.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Notifications {
-
+  status = 'unread';
+  fromDateControl = new FormControl<Date | null>(null);
+  onDateInput(event: any): void {
+    if (event.value) {
+      const selectedDate = event.value.toISOString().split('T')[0];
+      console.log('From date:', selectedDate); // Use for filtering, e.g., ECG reports
+    }
+  }
 }

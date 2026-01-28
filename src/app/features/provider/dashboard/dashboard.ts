c284144e7b1a99d27f78b63eeccf25c73ce9cabf
thoratprashant <prashant.thorat@arkenea.com>
2026-01-28 18:31:26 +0530

    Working on Provider Dahsboard

--- src/app/features/provider/dashboard/dashboard.ts
+++ src/app/features/provider/dashboard/dashboard.ts
@@ -1,8 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [MatIconModule,CommonModule,MatButtonModule,MatDialogModule,MatCheckboxModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })

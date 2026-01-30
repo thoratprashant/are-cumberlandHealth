5ba27641116bc5b1a0c72f9a1a643a6e92f8a696
satyajitarkenea24 <satyajit@arkenea.com>
2026-01-30 11:33:52 +0530

    ui

--- src/app/features/frontdesk/appointment-details-notes/appointment-details-notes.ts
+++ src/app/features/frontdesk/appointment-details-notes/appointment-details-notes.ts
@@ -1,18 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, viewChild, QueryList, ViewChildren, ViewChild, ElementRef } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatIconModule } from '@angular/material/icon'; 
+import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
-import { MatExpansionModule } from '@angular/material/expansion';
+import { MatAccordion, MatExpansionModule, MatExpansionPanel } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-appointment-details-notes',
-  imports: [  MatIconModule, CommonModule, MatButtonModule, MatDialogModule, MatCheckboxModule, MatTabsModule, MatExpansionModule],
+  imports: [MatIconModule, CommonModule, MatButtonModule, MatDialogModule, MatCheckboxModule, MatTabsModule, MatAccordion, MatExpansionModule],
   templateUrl: './appointment-details-notes.html',
   styleUrl: './appointment-details-notes.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppointmentDetailsNotes {
 
+  @ViewChild('cardAccordion', { static: false }) accordionRef!: ElementRef;
+
+  accordionId = 'cardAccordion';
+
+
+  ngAfterViewInit() {
+
+  }
 }
